4d0e1dae0280a24c8a02bd375841fe2b15937458
knowakowski <knowakowski@holidaycheck.com>
2017-05-12 17:39:09 +0200

    step 2nd

--- app.jsx
+++ app.jsx
@@ -1,6 +1,41 @@
 function Application() {
     return(
-        <div></div>
+        <div className="scoreboard">
+          <div className="header">
+            <h1>Scoreboard</h1>
+          </div>
+
+          <div className="players">
+            <div className="player">
+              <div className="player-name">
+                Jim Hoskins
+              </div>
+              <div className="player-score">
+                <div className="counter">
+                  <button className="counter-action decrement"> - </button>
+                  <div className="counter-score">
+                    31
+                  </div>
+                  <button className="counter-action increment"> + </button>
+                </div>
+              </div>
+            </div>
+            <div className="player">
+              <div className="player-name">
+                Jim Hoskins2
+              </div>
+              <div className="player-score">
+                <div className="counter">
+                  <button className="counter-action decrement"> - </button>
+                  <div className="counter-score">
+                    31
+                  </div>
+                  <button className="counter-action increment"> + </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     );
 }
 
