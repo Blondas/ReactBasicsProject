c2a678e7da155985f0b8d0cc2d75885949431920
knowakowski <knowakowski@holidaycheck.com>
2017-05-13 17:09:41 +0200

    component class

--- app.jsx
+++ app.jsx
@@ -8,6 +8,16 @@ let PLAYERS = [
       name: "Krzysztof Nowakowski",
       score: 3,
       id: 2,
+    },
+    {
+        name: "Ola Domańska",
+        score: 13,
+        id: 3,
+    },
+    {
+        name: "Szpon",
+        score: 666,
+        id: 4,
     }
 ];
 
@@ -23,21 +33,23 @@ Header.PropTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
-function Counter(props) {
-  return(
-      <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <div className="counter-score">
-            {props.score}
-        </div>
-        <button className="counter-action increment"> + </button>
-      </div>
-  );
-}
+let Counter = React.createClass({
+  propTypes: {
+      score: React.PropTypes.number.isRequired
+  },
+  render: function () {
+      return(
+          <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <div className="counter-score">
+                {this.props.score}
+            </div>
+            <button className="counter-action increment"> + </button>
+          </div>
+      );
+  }
+});
 
-Counter.PropsTypes = {
-  score: React.PropTypes.number.isRequired
-};
 
 function Player(props) {
   return(
